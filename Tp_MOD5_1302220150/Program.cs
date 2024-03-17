17a4a43d431c9de94c4b54147d6799d9c01c177a
Haikal Risnandar <115600487+Heris56@users.noreply.github.com>
2024-03-17 15:22:48 -0700

    Add files via upload
    add HaloGeneric Class

--- Tp_MOD5_1302220150/Program.cs
+++ Tp_MOD5_1302220150/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection.Metadata;
+
+public class HaloGeneric
+{
+    public void sapaUser(String nama)
+    {
+        
+        Console.WriteLine("Halo User " + nama);
+    }
+}
+
+class program()
+{
+    static void Main(String[] args)
+    {
+        HaloGeneric haikal = new HaloGeneric();
+        Console.Write("Nama: ");
+        String nama = Console.ReadLine();
+        haikal.sapaUser(nama);
+    }
+}
