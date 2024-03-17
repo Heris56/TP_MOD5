dd7cc5a5fc2e497a8a6564abec90a68445baae9b
Haikal Risnandar <115600487+Heris56@users.noreply.github.com>
2024-03-17 15:29:00 -0700

    Add files via upload
    add class DataGeneric

--- Tp_MOD5_1302220150/Program.cs
+++ Tp_MOD5_1302220150/Program.cs
@@ -1,2 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Reflection.Metadata;
+
+public class HaloGeneric
+{
+    public void sapaUser(String nama)
+    {
+        
+        Console.WriteLine($"Halo user {nama}");
+    }
+}
+
+public class DataGeneric<T>
+{
+    public T data { get; set; }
+
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+    public void printData()
+    {
+        Console.WriteLine($"Data Yang Tersimpan Adalah: {data}");
+    }
+}
+
+class program()
+{
+    static void Main(String[] args)
+    {
+        HaloGeneric haikal = new HaloGeneric();
+        Console.Write("Nama: ");
+        String nama = Console.ReadLine();
+        haikal.sapaUser(nama);
+
+        DataGeneric<String> data_haikal = new DataGeneric<String>("1302220150");
+        data_haikal.printData();
+    }
+}
